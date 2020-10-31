0950e5328735a5c49ca9b0763c5f0c7e8c003cf5
Adriana Cabrera <adi_111@icloud.com>
2020-10-30 17:37:31 -0700

    Fix order problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_233936) do
+ActiveRecord::Schema.define(version: 2020_10_30_060308) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
