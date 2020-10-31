e3614253bb54b2be814d7c10a7bf2a4d48a52de3
Adriana Cabrera <adi_111@icloud.com>
2020-10-30 19:04:21 -0700

    last update running

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_060308) do
+ActiveRecord::Schema.define(version: 2020_10_27_233936) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
