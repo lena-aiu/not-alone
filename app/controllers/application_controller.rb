aa90fe131bdfd6a8b2eeed7701843ba22fa95c46
vagrant <vagrant@vagrant.vm>
2021-01-11 18:59:19 -0500

    Changed labels from Customer to Client. Commented stuff inside require_current_user method in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
   def require_current_user
-    unless current_user.role.include?("administrator") ||
-      current_user.role.include?("intern")
-      flash[:error] = "You must be logged in"
-      redirect_to root_path
-    end
-  end 
+    # unless current_user.role.include?("administrator") ||
+    #   current_user.role.include?("intern")
+    #   flash[:error] = "You must be logged in"
+    #   redirect_to root_path
+    # end
+  end
 end
