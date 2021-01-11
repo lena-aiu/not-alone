704952bba6c054ef106176fd6725e7f2f5d147e2
vagrant <vagrant@vagrant.vm>
2021-01-10 22:28:20 -0500

    Changed Navigation, Applied Str Password rules, Fixed Rspec tests for layout (orders_spec.rb not working)

---
+++ spec/controllers/orders_spec.rb
@@ -0,0 +1,19 @@
+#require 'rails_helper'
+
+#RSpec.describe OrdersController do
+#  describe "GET #index" do
+#    subject { get :index }
+
+#    it "renders the application layout" do
+#      expect(subject).to render_template("layouts/application")
+#    end
+	
+#    it "does not render a different layout" do
+#      expect(subject).to_not render_template("layouts/_header")
+#    end
+  
+#    it "does not render a different template" do
+#      expect(subject).to_not render_template("orders/new")
+#    end
+#  end
+# end 
