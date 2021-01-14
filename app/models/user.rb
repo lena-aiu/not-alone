f20060beba3d81ad933519eff30a9aaab5c20c20
vagrant <vagrant@vagrant.vm>
2021-01-13 19:08:58 -0500

    made changes with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 6..128
+         :recoverable, :rememberable, :validatable, password_length: 8..25
   validate :password_complexity
-  
+
   def password_complexity
     if password.present? and not password.match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,25}$/)
-      errors.add :password, "must be at least 6 characters long and include 1 uppercase, 1 number, and 1 special character."
+      errors.add :password, "must be at least 8 characters long and include 1 uppercase, 1 number, and 1 special character."
     end
   end
 end
