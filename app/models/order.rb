7b7edc9e347f1d5cd10388c544795f526b5d975a
Adriana Cabrera <adi_111@icloud.com>
2021-02-03 18:45:28 -0800

    restoring the order files

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :service
   validates :description, presence: true
-  belongs_to :category
   #accepts_nested_attributes_for :customer
 end
