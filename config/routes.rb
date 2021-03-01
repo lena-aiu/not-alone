ee09421ec76301187f609062b8243cdbfa60a615
Lena Aiupova <e.ayupova@gmail.com>
2021-02-28 21:27:20 -0500

    removed links and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/index'
   get 'home/about'
-  get 'assignments/index'
   get 'home/health'
   post 'home/results', to: 'home#results', as: 'results'
   get 'home/results', to: 'home#results'
