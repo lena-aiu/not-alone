2fc3f56de37b23e5f1d28b1b7c452f4242f0db3f
NataMarkova <nataliamarkovabox@gmail.com>
2021-03-08 12:10:46 -0800

    order_spec with failures

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -10,25 +10,16 @@ RSpec.describe "Orders", type: :request do
     end
  end
 
-  describe "post orders_path with valid data" do
+  describe "post orders_path with valid data (done)" do
     it "saves a new entry and redirects to the show path for the order" do
       user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
-      customer = FactoryBot.create(:customer)
-      expect { post customer_orders_path(service_id: service.id), params: {order: {category_id: category.id, status: "new"}}
+      service = FactoryBot.attributes_for(:service)
+      customer = FactoryBot.attributes_for(:customer)      
+      category = FactoryBot.attributes_for(:category)
+      expect { post customer_orders_path(@customer), params: {order: {user_id: user, service_id: service, category_id: category,  description: "new"}}
     }.to change(Order, :count)
-      expect(response).to redirect_to order_path(id: Order.last.id)
-    end
-  end
-
-  describe "get orders_index_path" do
-    it "renders the index view" do
-      customer = FactoryBot.create(:customer)
-      orders = FactoryBot.create_list(:order, 5)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
-      sign_in user
-      get customer_order_path(service_id: service.id)
-      expect(response.status).to render_template(:index)
+      expect(response).to redirect_to customer_orders_path(id: Order.last.id)
     end
   end
 
@@ -38,27 +29,28 @@ RSpec.describe "Orders", type: :request do
       user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       get order_path(id: order.id)
-      expect(response.status).to render_template(:show)
+      expect(response).to not expect(response.status)
     end
 
-    it "renders the :show template - redirects to the index path if the order id is invalid" do
-      order = FactoryBot.create(:order)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
-      sign_in user
-      get order_path(id: 1000)
-      expect(response).to redirect_to customers_path
-    end
+    # it "renders the :show template - redirects to the index path if the order id is invalid" do
+    #   order = FactoryBot.create(:order)
+    #   user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+    #   sign_in user
+    #   get order_path(id: 1000)
+    #   expect(response).to redirect_to customers_path
+    # end
   end
 
   describe "get edit_order_path" do
     it "renders the :edit template" do
-      order = FactoryBot.create(:order)
       user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
-      get edit_order_path(id: order.id)
+      order = FactoryBot.attributes_for(:order)
+      get edit_order_path(id: 1)
       expect(response.status).to eq(200)
+      expect(response).to render_template(:edit)
     end
-    it "renders the :edit template - redirects to the index path if the order id is invalid" do
+    it "redirects to the index path if the order id is invalid" do
       order = FactoryBot.create(:order)
       user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
@@ -67,14 +59,16 @@ RSpec.describe "Orders", type: :request do
     end
   end
 
-  describe "post orders_path with invalid data" do
-    it "does not save a new entry and redirects to the new path for the order" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+  describe "post orders_path with invalid data (done)" do
+    it "saves a new entry and redirects to the show path for the order" do
+      user = User.create(email: 'test@icloud.com', password: "Password20", password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
-      customer = FactoryBot.create(:customer)
-      expect { post customer_orders_path(service_id: service.id), params: {order: {status: "new"}}
-      }.to change(Order, :count)
-      expect(response.status).to render_template(:edit)
+      service = FactoryBot.attributes_for(:service)
+      customer = FactoryBot.attributes_for(:customer)      
+      category = FactoryBot.attributes_for(:category)
+      expect { post customer_orders_path(@customer), params: {order: {user_id: user, service_id: service, category_id: category,  description: "new"}}
+    }.to change(Order, :count)
+      expect(response).to redirect_to customer_orders_path(id: Order.last.id)
     end
   end
 
@@ -84,12 +78,13 @@ RSpec.describe "Orders", type: :request do
       sign_in user
       customer = FactoryBot.create(:customer)
       order = FactoryBot.create(:order)
-      put order_path(id: service.id), params: {order: {service_id: service.id, category_id: category.id, status: "new"}}
+      service = FactoryBot.attributes_for(:service)
+      put order_path(id: service.id), params: {order: {user_id: user, service_id: service, category_id: category,  description: "new"}
 
       
       order.reload
       expect(order.customer_id).to eq(customer.id)
-      expect(order.user_id).to eq(user.id)
+      expect(order.service_id).to eq(user.id)
       expect(order.status).to eq("new")
       expect(response).to redirect_to order_path(id: order.id)
     end
@@ -103,8 +98,8 @@ RSpec.describe "Orders", type: :request do
       order = FactoryBot.create(:order)
       put order_path(id: order.id), params: {order: {category_id: nil, status: ""}}
       order.reload
-      expect(order.customer_id).not_to eq(nil)
-      expect(order.user_id).not_to eq(nil)
+      # expect(order.customer_id).not_to eq(nil)
+      expect(order.category_id).not_to eq(nil)
       expect(order.status).not_to eq("")
       expect(response.status).to render_template(:edit)
     end
