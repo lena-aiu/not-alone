1eeb697f061278f9c71c9d460758606d705c5347
Lena Aiupova <e.ayupova@gmail.com>
2021-03-15 13:46:05 -0400

    On NavBar still have link on Clients for Users without roles. continue working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,17 @@ module ApplicationHelper
 
     def is_user_authorized?
         if intern? || admin?
-          return 
+          return
         else
           flash[:error] = "You are not authorize for this operation."
           redirect_to services_path
         end
     end
+
+    # def active_tab(path)
+    #   request.path.match(/^#{path}/) ? { :class => 'active' } : {}
+    # end
+
+
 
 end
