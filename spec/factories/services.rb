96000f3626db0ecaafaa2c1664ba305835dc3dc6
Lena Aiupova <e.ayupova@gmail.com>
2021-03-21 17:34:00 -0400

    changed factory file

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -5,8 +5,17 @@ FactoryBot.define do
     name { Faker::Lorem.unique.word }
     description {"MyDesc"}
     kind {"any"}
-    phone_number {1234567890}
+    f.phone { Faker::Number.number(digits: 10) }
     #url {"www.example.org"}
     #picture {"uno.jpeg"}
+    f.street { Faker::Address.street_address }
+    f.city { Faker::Address.city }
+    f.state { Faker::Address.state }
+    f.zip { Faker::Number.number(digits: 5) }
+    # t.float "latitude"
+    # t.float "longitude"
+    f.latitude { Faker::Address.latitude } #=> "-58.17256227443719"
+    f.longitude { Faker::Address.longitude } #=> "-156.65548382095133"
+
   end
 end
