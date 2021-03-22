55bdb357fc7e551c5fbfd61acb3670c905ad13a5
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-03-22 16:05:25 -0400

    added checkbocks for user roles

--- app/controllers/user_rolles_controller.rb
+++ app/controllers/user_rolles_controller.rb
@@ -8,10 +8,7 @@ class UserRollesController < ApplicationController
     helper_method :intern?
 
     def index
-        if current_user.role.nil? || current_user.role.include?("volunteer")
-          flash.notice = "You are not authorized for that operation."
-          redirect_to home_index_path
-        elsif current_user.role.include?("administrator") || current_user.role.include?("intern")
+        if current_user.role&.include?("administrator") || current_user.role&.include?("intern")
           @user_rolles = User.all
         else
           flash.notice = "You are not authorized for that operation."
@@ -20,10 +17,7 @@ class UserRollesController < ApplicationController
       end
 
     def show
-    if current_user.role.nil? || current_user.role.include?("volunteer")
-      flash.notice = "You are not authorized for that operation."
-      redirect_to home_index_path
-    elsif current_user.role.include?("administrator") || current_user.role.include?("intern")
+      if current_user.role&.include?("administrator") || current_user.role&.include?("intern")
       render :show
     else
       flash.notice = "You are not authorized for that operation."
@@ -32,10 +26,11 @@ class UserRollesController < ApplicationController
   end
   
   def edit
+
     if current_user.role.nil?
       flash.notice = "You are not authorized for that operation."
       redirect_to home_index_path
-    elsif current_user.role.include?("administrator") || current_user.role.include?("intern")
+    elsif current_user.role.include?("administrator")
       render :edit
     else
       flash.notice = "You are not authorized for that operation."
@@ -47,7 +42,7 @@ class UserRollesController < ApplicationController
     if current_user.role.nil?
       flash.notice = "You are not authorized for that operation."
       redirect_to home_index_path
-    elsif current_user.role.include?("administrator") || current_user.role.include?("intern")
+    elsif current_user.role.include?("administrator")
       @user_rolle.destroy
     else
       flash.notice = "You are not authorized for that operation."
@@ -59,9 +54,47 @@ class UserRollesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def update
+    if current_user.role.nil?
+      flash.notice = "You are not authorized for that operation."
+      redirect_to home_index_path
+    elsif current_user.role.include?("administrator")
+      role=""
+      if params["volunteer"] == "1"
+        role = "volunteer"
+      end
+      if params["intern"] == "1" 
+        if role == ""
+          role = "intern"
+        else 
+          role += ",intern"
+        end
+      end
+      if params["administrator"] == "1" 
+        if role == ""
+          role = "administrator"
+        else 
+          role += ",administrator"
+        end
+      end
+
+      @user_rolle.update(role: role)
+      flash.notice = "The user record was updated successfully."
+      byebug 
+      redirect_to user_rolle_path(id: @user_rolle.id)
+    else
+      flash.notice = "You are not authorized for that operation."
+      redirect_to home_index_path
+    end
+  end
   private
   def set_user_rolle
     @user_rolle = User.find(params[:id])
+  end
+  def catch_not_found(e)
+    Rails.logger.debug("We had a not found exception.")
+    flash.alert = e.to_s
+    redirect_to user_rolles_path
   end
 end
 
