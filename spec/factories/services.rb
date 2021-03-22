759694a6a35ae2e7f6e5612f2679fd5f0210f051
Lena Aiupova <e.ayupova@gmail.com>
2021-03-22 13:12:08 -0400

    Fixed service, assignment test. Added new tests for video and customer. Changes buttons names from Edit to Update for Customer, Video, Categories

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -1,21 +1,16 @@
 require 'faker'
 
 FactoryBot.define do
-  factory :service do
+  factory :service do |f|
     name { Faker::Lorem.unique.word }
     description {"MyDesc"}
     kind {"any"}
-    f.phone { Faker::Number.number(digits: 10) }
+    f.phone_number { Faker::Number.number(digits: 10) }
     #url {"www.example.org"}
     #picture {"uno.jpeg"}
     f.street { Faker::Address.street_address }
     f.city { Faker::Address.city }
     f.state { Faker::Address.state }
     f.zip { Faker::Number.number(digits: 5) }
-    # t.float "latitude"
-    # t.float "longitude"
-    f.latitude { Faker::Address.latitude } #=> "-58.17256227443719"
-    f.longitude { Faker::Address.longitude } #=> "-156.65548382095133"
-
   end
 end
--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -7,7 +7,8 @@ end
 RSpec.describe "Videos", type: :request do
   describe "sign in" do
     it "signs user in and out" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       get root_path
       expect(response).to render_template(:index)
@@ -27,33 +28,42 @@ RSpec.describe "Videos", type: :request do
   describe "get video_path" do
     it "renders the :show template" do
       video = FactoryBot.create(:video)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       get video_path(id: video.id)
       expect(response.status).to render_template(:show)
     end
     it "renders the :show template - redirects to the index path if the video id is invalid" do
       video = FactoryBot.create(:video)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       get video_path(id: 5000)
       expect(response).to redirect_to videos_path
     end
   end
 
   describe "get new_video_path" do
-    it "renders the :new template" do
+    it "renders the :new template for an administrator role" do
       video = FactoryBot.create(:video)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       get new_video_path
       expect(response).to be_successful
       expect(response).to render_template(:new)
+    end
+    it "redirects to the new_user_session_path if a user is not logged in" do
+      service = FactoryBot.create(:service)
+      get new_video_path
+      expect(response).to redirect_to new_user_session_path
     end
   end
 
   describe "get new_video_path" do
     it "renders the :new template - redirects to the index path if the the user role is invalid" do
       video = FactoryBot.create(:video)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "stranger")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "stranger")
       sign_in user
       get new_video_path
       expect(response).to_not render_template(:new)
@@ -63,7 +73,8 @@ RSpec.describe "Videos", type: :request do
   describe "get edit_video_path" do
     it "renders the :edit template" do
       video = FactoryBot.create(:video)
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       get edit_video_path(id: video.id)
       expect(response.status).to render_template(:edit)
@@ -76,7 +87,8 @@ RSpec.describe "Videos", type: :request do
 
   describe "post videos_path with valid data" do
     it "saves a new entry and redirects to the show path for the video" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       video_attributes = FactoryBot.attributes_for(:video)
       expect { post videos_path, params: {video: video_attributes}
@@ -87,7 +99,8 @@ RSpec.describe "Videos", type: :request do
 
   describe "post videos_path with invalid data" do
     it "does not save a new entry and redirects to the new path for the video" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       video_attributes = FactoryBot.attributes_for(:video)
       video_attributes.delete(:title)
@@ -99,7 +112,8 @@ RSpec.describe "Videos", type: :request do
 
   describe "put video_path with valid data" do
     it "updates an entry and redirects to the show path for the video" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       video = FactoryBot.create(:video)
       put video_path(id: video.id), params: {video: {title: "new", description: "new", text: "new"}}
@@ -111,7 +125,8 @@ RSpec.describe "Videos", type: :request do
 
   describe "put video_path with invalid data" do
     it "updates an entry and redirects to the edit path for the video" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+        password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       video = FactoryBot.create(:video)
       put video_path(id: video.id), params: {video: {title: "", description: "", text: ""}}
@@ -123,10 +138,11 @@ RSpec.describe "Videos", type: :request do
 
   describe "delete a video record" do
     it "deletes a video record and redirects to the index path" do
-      user = User.create(email: 'test@icloud.com', password: "Pa$$word20", password_confirmation: "Pa$$word20", role: "administrator")
+      user = User.create(email: 'test@icloud.com', password: "Pa$$word20",
+      password_confirmation: "Pa$$word20", role: "administrator")
       sign_in user
       video = FactoryBot.create(:video)
-      delete video_path(id: video.id), params: {video:{title: "new", description: "new", text: "new"}}
+      expect {delete video_path(id: video.id)}.to change(Video, :count)
       expect(response).to redirect_to videos_path
      end
   end
